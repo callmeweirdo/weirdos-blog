1b5d22bb21971e3abade0ff05595ed37567359e3
Realest Weirdo <ogbandav102@gmail.com>
2022-04-11 00:49:35 +0100

    working a laravel 8 blog site.. already created few controllers and the blog homepage

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,72 @@
+    <footer class="bg-gray-800 py-20 mt-20">
+        <div class="sm:grid grid-cols-3 pb-10 w-4/5 m-auto border-b-2 border-red-800 ">
+            <div class="">
+                <h3 class="text-l sm:font-bold text-gray-100 ">
+                    Pages
+                </h3>
+
+                <ul class="sm:text-s py-4 pt-4 text-gray-400 ">
+                    <li class="pb-1">
+                        <a href="/">Home</a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/login">Blog</a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/login">Login</a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/register">Register</a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class=" ">
+                <h3 class="text-l sm:font-bold text-gray-100 ">
+                    Find Us
+                </h3>
+
+                <ul class="sm:text-s py-4 pt-4 text-gray-400 ">
+                    <li class="pb-1">
+                        <a href="/">What We Do</a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/">Address</a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/">Phone</a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/">Contact</a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <div class=" ">
+                <h3 class="text-l sm:font-bold text-gray-100 ">
+                    Latest Posts
+                </h3>
+
+                <ul class="sm:text-s py-4 pt-4 text-gray-400 ">
+                    <li class="pb-1">
+                        <a href="/">Why we Love Tech</a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/login">Why we Love Design </a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/login">Why we use Laravel</a>
+                    </li>
+                    <li class="pb-1">
+                        <a href="/register">Why PHP is the best</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6 ">
+        Copyright 2100-2101 weirdo space. All rigth reserved
+    </p>
+
+</footer>
