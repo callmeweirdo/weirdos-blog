7716d805a2bf28bd5f4864bac1c570c3f13565cc
Realest Weirdo <ogbandav102@gmail.com>
2022-04-13 12:13:06 +0100

    made the blogs to display on he blog page and onClinkig on each blog navigates to the individual blog post page and as well as the editing and deletion of each blog post

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,16 +9,29 @@
         </div>
     </div>
 
+    @if (session()->has('message'))
+        <div class="w-4/5 m-auto mt-10 pl-2 text-center">
+            <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl py-4 px-4">
+                {{ session()->get('message') }}
+            </p>
+        </div>
+    @endif
+
+
+
+
     @if (Auth::check())
         <div class="pt-15 w-4/5 m-auto ">
-            <a href="/blog/create" class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs py-3 px-5 font-exrabold rounded-3xl ">create post</a>
+            <a href="/blog/create"
+                class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs py-3 px-5 font-exrabold rounded-3xl ">create
+                post</a>
         </div>
     @endif
 
     @foreach ($posts as $post)
         <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200 ">
             <div>
-                <img src="https://cdn.pixabay.com/photo/2018/09/29/04/30/planet-3710757_960_720.jpg" width="700" alt="">
+                <img src="{{ asset('images/' . $post->image_path) }}" width="700" alt="">
             </div>
             <div>
                 <h2 class="text-gray-700 font-bold text ">{{ $post->title }}</h2>
@@ -26,14 +39,35 @@
                 <span class="textt-gray-500">
                     By <span class="font-bold-800 italic font-bold ">
                         {{ $post->user->name }}
-                    </span>, Created on {{ date('jS M Y', strtotime($post->created_on) ) }}
+                    </span>, Created on {{ date('jS M Y', strtotime($post->created_on)) }}
                 </span>
 
                 <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
                     {{ $post->description }}
                 </p>
 
-                <a href="" class="uppercase bg-blue-500 text-gray-100 font-extrabold py-4 px-8 rounded-3xl ">keep readng</a>
+                <a href="blog/{{ $post->slug }}"
+                    class="uppercase bg-blue-500 text-gray-100 font-extrabold py-4 px-8 rounded-3xl ">keep readng
+                </a>
+
+                @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                    <span class="float-right">
+                        <a href="/blog/{{ $post->slug }}/edit" class="text-gray-700 hover:text-gray-900 pb-1 border-b-2 ">
+                            Edit
+                        </a>
+                    </span>
+
+                    <span class="float-right">
+                        <form action="/blog/{{ $post->slug }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button class="text-red-500 pr-3" type="submit">
+                                Delete
+                            </button>
+                        </form>
+                    </span>
+                @endif
+
             </div>
         </div>
     @endforeach
